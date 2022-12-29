0b04e4f7bca4dfd7e64e485bf3a7643f9384f400
SadiqAgayev <sadiqagayev820@gmail.com>
2022-12-29 20:44:44 +0400

    getCategories

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
+import React, { Component } from "react";
 import { Badge, Col, Container, Row } from "reactstrap";
 import CategoryList from "./components/CategoryList";
 import Navi from "./components/Navi";
 import ProductList from "./components/ProductList";
 
-function App() {
+export default class App extends Component {
+  state = {
+    categories: [],
+    currentCategory: "",
+  };
 
-  return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="4">
-            <Badge color="danger" style={{width: "100%"}}>
-              <CategoryList title="CategoryList" />
-            </Badge>
-          </Col>
-          <Col xs="8">
-            <Badge color="primary" style={{width: "100%"}}><ProductList /></Badge>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+
+  componentDidMount() {
+    this.getCategories()
+  }
+
+  getCategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then((response) => response.json())
+      .then((responseJson) => this.setState({ categories: responseJson }));
+  };
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Navi color="dark" dark={true} expand="sm" />
+          </Row>
+          <Row className="mt-2">
+            <Col xs="4">
+              <Badge color="danger" style={{ width: "100%" }}>
+                <CategoryList
+                  categories={this.state.categories}
+                  currentCategory={this.state.currentCategory}
+                  changeCategory={this.changeCategory}
+                />
+              </Badge>
+            </Col>
+            <Col xs="8">
+              <Badge color="primary" style={{ width: "100%" }}>
+                <ProductList />
+              </Badge>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -2,29 +2,24 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-    state = {
-        categories: [
-            {categoryId: 1, categoryName: "Beverages"},
-            {categoryId: 2, categoryName: "Condiments"},
-            {categoryId: 3, categoryName: "Meals"}
-        ],
-        currentCategory: ""
-    }
-
-    changeCategory = (category) => {
-        this.setState({currentCategory: category.categoryName})
-    }
-
   render() {
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>CategoryList</h1>
         <ListGroup>
-            {this.state.categories.map(category => {
-                return <ListGroupItem active={category.categoryName===this.state.currentCategory} onClick={() => this.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
-            })}
+          {this.props.categories.map((category) => {
+            return (
+              <ListGroupItem
+                active={category.categoryName === this.props.currentCategory}
+                onClick={() => this.props.changeCategory(category)}
+                key={category.categoryId}
+              >
+                {category.categoryName}
+              </ListGroupItem>
+            );
+          })}
         </ListGroup>
-        <h2>{this.state.currentCategory}</h2>
+        <h2>{this.props.currentCategory}</h2>
       </div>
     );
   }
